0fcb5c5d79956e6e951d947f93817d64efd732e2
Mark Mestern <mark.mestern@rvu.co.uk>
2024-07-12 18:53:17 +0100

    fastest

--- src/countdown/core.clj
+++ src/countdown/core.clj
@@ -89,28 +89,37 @@
   [numbers shape]
   (cond
     (and
-     (= :n shape)
+;     (= :n shape)
      (= 1 (count numbers))) (first numbers)
     :else (let [[op n left right]  shape]
             (list op
                   (populate-tree (take n numbers) left)
                   (populate-tree (drop n numbers) right)))))
 
-;(trace/trace-vars populate-tree evaluate)
+;(trace/trace-vars populate-tree* evaluate)
 
 (def populate-tree (memoize populate-tree*))
 
 (defn populate-trees
   [combination shape]
   ;; (println "populate-trees call" combination (pprint shape))
-  ;; TODO create a function to eval.
   (let [permuations (combo/permutations combination)
         r (for [p permuations]
             (populate-tree p shape))]
     r
     ))
 
-(defn try-combination-2
+#_(defn make-fn-1
+  [tree num-vars]
+  ;; LOOK HERE: Fill in code below to return a function that takes num-vars vars and substitutes them in the places currently represented by :n
+  (let [zipper (zip/seq-zip tree)]
+    (fn [& args]
+      (-> zipper
+          (zip/postwalk #(if (= % :n) (first args) %)) ;; replace each occurrence of `:n` with the first arg, then move on to the next arg
+          zip/root))))
+
+
+#_(defn try-combination-2
   [target combination]
   (let [shapes          (gen-shapes (count combination))
 ;        _               (println "shapes" shapes)
@@ -125,21 +134,75 @@
 (defn try-combination
   [target combination]
   (let [trees (gen-trees combination)
-        best (first (eval-all target trees))]
+        best  (first (eval-all target trees))]
     (println  "Using number set " combination  " =>   Result" (pprint best))
     (when (zero? (:score best))
       (System/exit 0))))
 
+(defn get-function-for-shape
+  "Return a function that provides score for combination for this shape "
+  [shape target num-args]
+  (let [args            (mapv #(symbol (str "arg_" %)) (range num-args))
+  ;      _               (println "args" args)
+        shape-with-args (populate-tree* args shape)
+;        _               (println "get-function-for-shape shape-with-args" shape-with-args)
+        shaped-fn       (eval (concat '(fn) [args] [shape-with-args]))]
+ ;   (println "get-function-for-shape shaped-fn 1 " (concat '(fn) [args] [shape-with-args]))
+ ;   (println "get-function-for-shape shaped-fn 2" shaped-fn)
+    (fn [args]
+      (let [value (try
+                    (apply shaped-fn args)
+                    (catch ArithmeticException e ;; div by 0
+                      nil))]
+        (merge
+         (when (= target value)
+           {:message "!!!!! WINNER !!!"})
+         {:tree   (populate-tree* args shape)
+          :target target
+          :score  (when value (abs (- target value)))
+          :value  value})))))
+
+(defn method-3
+  [target nums]
+  (doall
+   (reduce
+    (fn [a i]
+      (if (or
+           (nil? a)
+           (and
+            (:score i)
+            (< (:score i) (:score a))))
+        (do
+          (print "\nNew best" (pprint i) "\n")
+          (flush)
+          (if (zero? (:score i))
+            (reduced i)
+            i))
+        a))
+    nil
+    (apply concat
+           (for [i (range 1 (inc (count nums)))]
+             (do (println "loop i" i )
+                 (apply concat
+                        (for [shape (gen-shapes i)]
+                          (let [f (get-function-for-shape shape target i)]
+                            (map f (combo/permuted-combinations nums i)))))))))))
+
 (defn -main
   [target & nums]
+  ;;  method 3
   (let [target (Integer/parseInt target)
         nums   (map #(Integer/parseInt %) nums)]
     (println "Aiming for" target "with" nums)
-    (let [combinations (for [n           (range 1 (inc (count nums)))
+    (method-3 target nums))
+  ;; method 2
+  #_(let [combinations (for [n           (range 1 (inc (count nums)))
                              combination (combo/combinations nums n)]
                          combination)]
-      (doall (map #(try-combination-2 target %) combinations))))
-  ;; Old method
+      (doall (map #(try-combination-2 target %) combinations)))
+
+
+  ;;  method 1
   #_(when-not true
     (let [target (Integer/parseInt target)
           nums   (map #(Integer/parseInt %) nums)]
