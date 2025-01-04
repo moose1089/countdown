a6ff23af18af8c38ba92d00e3558a57b19228112
mark <mark.mestern@gmail.com>
2025-01-04 18:22:24 +0000

    daily commit

--- src/countdown/core_4.clj
+++ src/countdown/core_4.clj
@@ -1,4 +1,4 @@
-(ns countdown.core-3
+(ns countdown.core-4
   (:require [clojure.math.combinatorics :as combo]
             [clojure.pprint :as pprint]
             [clojure.tools.trace :as trace]
@@ -6,8 +6,8 @@
             [clojure.walk :as walk])
   (:gen-class))
 
-;; lein v3 676  500 100 5 3 2 1
-;; lein v3 930 50 75 10 5 1 7
+;; lein v4 676  500 100 5 3 2 1
+;; lein v4 930 50 75 10 5 1 7
 
 (defn pprint [s]
   (let [reps {* "*" + "+" - "-" / "/"}
@@ -34,16 +34,22 @@
 
 (def remove-all (memoize remove-all*))
 
-(defn make-initial-vals [target nums n]
+(defn make-initial-vals
+  [target nums n]
+  {:post [(:expr %)]}
   {:v           n
+   :expr        (str n)
    :leaf        true
    :nums-avail  []
    :score       (score-for target n)})
 
-(defn state-score [s]
+(defn state-score
+  [s]
+  {:post [(:expr %)]}
   (let [best (first (sort-by :score (:nums-avail s)))]
     (assoc s
            :v (:v best)
+           :expr (:expr best)
            :score (:score best))))
 
 (defn starting-state [target initial-values]
@@ -52,9 +58,9 @@
 (defn next-states [target current-state]
   (let [r
         (filter some?
-                (when (not-empty? (:nums-avail b))
+                (when (not (empty? (:nums-avail current-state)))
                   (for [op    [+ - * /]
-                        [a b] (combo/combinations (:nums-avail b) 2)
+                        [a b] (combo/combinations (:nums-avail current-state) 2)
                         [l r] [[a b] [b a]]]
                     (when (and
                            (not (and (= op /) (zero? (:v r))))                     ;; no divide by 0
@@ -62,11 +68,11 @@
                            (not (and (= op /) (not (zero? (mod (:v l) (:v r))))))) ;; stick to ℤ
                       (let [v       (op (:v l) (:v r))
                             new-num {:v     v
-                                     :left  l
-                                     :right r
+                                     :expr  (str "(" (:expr l) (pprint op) (:expr r) ")")
+                                     :op    op
                                      :score (score-for target v)}]
-                        (state-score {:nums-avail (conj (remove-all (:nums-avail) [l r]) new-num)}))))))]
-    (println "next-states" target "from" current-state "=>" (pprint r))
+                        (state-score {:nums-avail (conj (remove-all (:nums-avail current-state) [l r]) new-num)}))))))]
+    #_(println "next-states" target "from" current-state "=>"  r)
     r))
 
 #_(defn unique-by
@@ -84,32 +90,34 @@
 
 (defn solve
   ([target nums]
-   (swap! profile inc)
+
    ;(println "TRACE solve" (pprint [target nums energy]))
-   (let [grouping-fn    (juxt :v :nums-used)
+   (let [grouping-fn    (juxt :v :nums-avail)
+         has-2-nums?    #(< 1(count (:nums-avail %)))
          unseen-fn      (fn [found-values new-vals]
                           (remove #(get-in found-values (grouping-fn %)) new-vals))
          initial-values (map (partial make-initial-vals target nums) nums)
-         start          (starting-state initial-values)]
+         start          (starting-state target initial-values)]
      (loop [found-values   {} ;; {[:nums-avail :v] value}
             states-waiting [start]
-            best-value     nil])
-     (cond (or
-            (zero? (or (:score best-value) 1))
-            (empty? states-waiting))
-           best-value
-           :else (let [c                (first states-waiting)
-                       candidate-states (sort-by :score (unseen-fn found-values (next-states target c)))]
-                   (recur
-                    (merge found-values (group-by grouping-fn candidate-states))
-                    (concat (rest states-waiting) candidate-states)
-                    (best-of best-value (first candidate-states)))
-                   )))))
+            best-value     nil]
 
-(trace/trace-vars #_s-contains #_next-states
-                  #_value-position
-                  #_remove-first*
-                  )
+       (swap! profile inc)
+       (when (zero? (mod @profile 1000))
+         (println "found" (count found-values) "waiting" (count states-waiting)
+                                        ;"best score" (:score best-value) ":" (:expr best-value) "=" (:v best-value)
+                  ))
+       (cond (or
+              (zero? (or (:score best-value) 1))
+              (empty? states-waiting))
+             best-value
+             :else (let [c                (first states-waiting)
+                         candidate-states (sort-by :score (unseen-fn found-values (next-states target c)))]
+                     (recur
+                      (merge found-values (group-by grouping-fn candidate-states))
+                      (concat (rest states-waiting) (filter has-2-nums? candidate-states))
+                      (best-of best-value (first candidate-states)))
+                     ))))))
 
 (defn -main
   [target & nums]
@@ -119,6 +127,6 @@
     (let [r (solve target nums)]
       (when (== 0 (:score r))
         (println "FOUND!"))
-      (println "BEST" (pprint r) "=" (:v r), "score=" (:score r))
+      (println "BEST" (:expr r) "=" (:v r), "score=" (:score r))
       (println "num calls" @profile)))
   (shutdown-agents))
